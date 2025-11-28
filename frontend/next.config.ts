7c527ec7477f4a55ec5e7e45f7865bff84b6a1d2
AdvxnSA3R <sakibchowdhury2008@gmail.com>
2025-11-28 11:34:09 +0000

    making sure GitHub pages knows the path

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/tadaaa',
 
   /* config options here */
   reactCompiler: true,
 };
+
 
 export default nextConfig;
